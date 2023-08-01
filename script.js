afabafbecbc77a69a8fdb3bdb92532d9062bd682
RedHood-08 <136153685+RedHood-08@users.noreply.github.com>
2023-08-01 15:33:03 +0800

    Add a playRound function
    Determine the winner

--- script.js
+++ script.js
@@ -1,5 +1,42 @@
 function getComputerChoice() {
-    const choices = ["Rock", "Paper", "Scissors"];
+    const choices = ["rock", "paper", "scissors"];
 
-    console.log(choices[Math.floor(Math.random()*3)]);
+    return choices[Math.floor(Math.random()*3)];
 }
+
+function playRound(playerSelection, computerSelection) {
+    if(playerSelection === computerSelection) {
+        return "Tie";
+    }else{
+        switch (playerSelection) {
+            case "rock":
+                if(computerSelection === "scissors") {
+                    console.log("You Win! Rock beats Scissors");
+                }else {
+                    console.log("You Lose! Paper beats Rock");
+                }
+                break;
+
+            case "paper":
+                if(computerSelection === "rock") {
+                    console.log("You Win! Paper beats Rock");
+                }else {
+                    console.log("You Lose! Scissors beats Paper");
+                }
+                break;
+
+            case "scissors":
+                if(computerSelection === "paper") {
+                    console.log("You Win! Scissors beats Paper");
+                }else {
+                    console.log("You Lose! Rock beats Scissors");
+                }
+                break;
+        }
+    }
+}
+
+const playerSelection = "rock";
+const computerSelection = getComputerChoice();
+
+console.log(playRound(playerSelection, computerSelection));
